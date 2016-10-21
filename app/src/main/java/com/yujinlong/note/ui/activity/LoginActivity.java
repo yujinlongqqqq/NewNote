6a76ecd1852e6ebe8707aa72279e92f3eb00ebc3
xiaoyu <1191962683@qq.com>
2016-10-21 13:50:08 +0800

    优化登录页

--- app/src/main/java/com/yujinlong/note/ui/activity/LoginActivity.java
+++ app/src/main/java/com/yujinlong/note/ui/activity/LoginActivity.java
@@ -1,10 +1,13 @@
 package com.yujinlong.note.ui.activity;
 
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import com.yujinlong.note.R;
 import com.yujinlong.note.view.OwlView;
@@ -15,7 +18,7 @@ import com.yujinlong.note.view.swipeback.BaseSwipeBackActivity;
  * A login screen that offers login via email/password.
  */
 public class LoginActivity extends BaseSwipeBackActivity {
-
+    private CheckBox mCheckbox;
     private OwlView mOwlView;
     private EditText email,password;
     private Button login;
@@ -23,10 +26,10 @@ public class LoginActivity extends BaseSwipeBackActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mCheckbox= (CheckBox) findViewById(R.id.lg_checkbox);
         mOwlView= (OwlView) findViewById(R.id.owl_view);
         email= (EditText) findViewById(R.id.email);
         password= (EditText) findViewById(R.id.password);
-        login= (Button) findViewById(R.id.btn_login);
         password.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
@@ -39,10 +42,16 @@ public class LoginActivity extends BaseSwipeBackActivity {
             }
         });
 
-        login.setOnClickListener(new View.OnClickListener() {
+        mCheckbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onClick(View v) {
-                Toast.makeText(LoginActivity.this,"我要登录了",Toast.LENGTH_SHORT).show();
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    if(isChecked){
+                        //如果选中，显示密码
+                        password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                    }else{
+                        //否则隐藏密码
+                        password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    }
             }
         });
 
